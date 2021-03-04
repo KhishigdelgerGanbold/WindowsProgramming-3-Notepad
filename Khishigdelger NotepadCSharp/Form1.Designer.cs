73b9fb28d984afb796ab2c0e7f40513775660de6
Khishigdelger Ganbold <weskera428@gmail.com>
2021-03-05 01:51:11 +0800

    The notepad can now save and create new files.
    saveFile(), createFile();

--- Khishigdelger NotepadCSharp/Form1.Designer.cs
+++ Khishigdelger NotepadCSharp/Form1.Designer.cs
@@ -79,6 +79,7 @@ namespace Khishigdelger_NotepadCSharp
             this.newToolStripMenuItem.Name = "newToolStripMenuItem";
             this.newToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
             // 
             // openToolStripMenuItem
             // 
--- Khishigdelger NotepadCSharp/Form1.cs
+++ Khishigdelger NotepadCSharp/Form1.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
     namespace Khishigdelger_NotepadCSharp
     {
         public partial class Form1 : Form
         {
+        private OpenFileDialog openfd;
+        private SaveFileDialog savefd;
+        private FontDialog fd;
             public Form1()
             {
                 InitializeComponent();
@@ -21,5 +25,54 @@ using System.Windows.Forms;
             {
 
             }
+
+        private void createFile()
+        {
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result;
+            if (!string.IsNullOrEmpty(this.richTextBox1.Text))
+                {
+                  result = MessageBox.Show("Шинэ файл нээхээс өмнө одоогийн файлаа хадгалах уу?\nYes - Хадгалах No - Хадгалахгүй", "Анхааруулга!", buttons);
+                if (result == DialogResult.Yes)
+                {
+                    saveFile();
+                }
+                else
+                {
+                    this.richTextBox1.Text = string.Empty;
+                    this.Text = "Untitled";
+                }
+            }
+            else
+                {
+                    this.richTextBox1.Text = string.Empty;
+                    this.Text = "Untitled";
+                }
         }
+
+        private void saveFile()
+        {
+            if (string.IsNullOrEmpty(this.richTextBox1.Text))
+            {
+                MessageBox.Show("Файл хоосон байна!", "Анхааруулга!");
+            }
+            else
+            {
+                savefd = new SaveFileDialog();
+                savefd.Title = "Save As";
+                savefd.Filter = "Text Document|*.txt";     
+                savefd.DefaultExt = "txt";
+
+                if(savefd.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllText(savefd.FileName, this.richTextBox1.Text);
+                }
+            }
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            createFile();
+        }
+    }
     }
